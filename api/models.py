dadd2c22ef7eed64a612defc7818c66ceaf0de83
jonas.voland <jonas.voland@students.ffhs.ch>
2024-05-03 22:18:44 +0200

    feat: set ticket number from last ticket number in queue

--- api/models.py
+++ api/models.py
@@ -15,7 +15,7 @@ class Ticket(models.Model):
     # Fields
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     queue = models.ForeignKey(Queue, on_delete=models.CASCADE)
-    number = models.IntegerField()
+    number = models.IntegerField(editable=False)
     status_choices = [(1, 'new'), (2, 'in progress'), (3, 'done')]
     status = models.IntegerField(choices=status_choices)
     createdAt = models.DateTimeField(auto_now_add=True)
@@ -27,6 +27,14 @@ class Ticket(models.Model):
     def save(self, *args, **kwargs):
         if self.status not in [choice[0] for choice in self.status_choices]:
             raise ValueError('Invalid status value.')
+        
+        if not self.pk:
+            last_ticket = Ticket.objects.filter(queue=self.queue).order_by('-number').first()
+            if last_ticket:
+                self.number = last_ticket.number + 1
+            else:
+                self.number = 1
+        
         super().save(*args, **kwargs)
 
     def __str__(self):
