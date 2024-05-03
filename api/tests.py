0acdd6df138de9f78029ea04014c30ca393b683f
jonas.voland <jonas.voland@students.ffhs.ch>
2024-05-03 20:38:49 +0200

    test: adding negative tests

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
 from api.models import Queue, Ticket
 
 class QueueModelTestCase(TestCase):
@@ -10,6 +11,20 @@ class QueueModelTestCase(TestCase):
     def test_queue_creation(self):
         self.assertEqual(self.queue.name, 'Test Queue')
         self.assertEqual(self.queue.user, self.user)
+
+class QueueModelNegativeTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser')
+
+    def test_queue_name_blank(self):
+        # Attempt to create a Queue with a blank name
+        with self.assertRaises(IntegrityError):
+            Queue.objects.create(user=self.user, name='')
+
+    def test_queue_user_null(self):
+        # Attempt to create a Queue with a null user
+        with self.assertRaises(IntegrityError):
+            Queue.objects.create(name='Test Queue')
 
 class TicketModelTestCase(TestCase):
     def setUp(self):
@@ -27,3 +42,23 @@ class TicketModelTestCase(TestCase):
         # Attempt to create a ticket with the same number within the same queue
         with self.assertRaises(Exception):
             Ticket.objects.create(user=self.user, queue=self.queue, number=1, status=2)
+
+class TicketModelNegativeTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser')
+        self.queue = Queue.objects.create(user=self.user, name='Test Queue')
+
+    def test_ticket_number_blank(self):
+        # Attempt to create a Ticket with a blank number
+        with self.assertRaises(IntegrityError):
+            Ticket.objects.create(user=self.user, queue=self.queue, number=None, status=1)
+
+    def test_ticket_status_invalid(self):
+        # Attempt to create a Ticket with an invalid status
+        with self.assertRaises(ValueError):
+            Ticket.objects.create(user=self.user, queue=self.queue, number=1, status=5)
+
+    def test_ticket_queue_null(self):
+        # Attempt to create a Ticket with a null queue
+        with self.assertRaises(IntegrityError):
+            Ticket.objects.create(user=self.user, number=1, status=1)
