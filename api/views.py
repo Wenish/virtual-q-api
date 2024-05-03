d0b2407db046b460fdc5af2b5b696a6bd4e98997
jonas.voland <jonas.voland@students.ffhs.ch>
2024-05-03 18:23:06 +0200

    feat: adding basic permissions

--- api/views.py
+++ api/views.py
@@ -1,11 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Queue, Ticket
 from .serializers import QueueSerializer, TicketSerializer
+from .permissions import IsOwner
 
 class QueueViewSet(viewsets.ModelViewSet):
     queryset = Queue.objects.all()
     serializer_class = QueueSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
