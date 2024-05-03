6a6fc247086c0d0f65e549a081309ab5d459f636
jonas.voland <jonas.voland@students.ffhs.ch>
2024-05-03 18:46:32 +0200

    Update permissions.py

--- api/permissions.py
+++ api/permissions.py
@@ -2,5 +2,4 @@ from rest_framework.permissions import BasePermission
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        # Check if the user making the request is the owner of the ticket
         return obj.user == request.user
