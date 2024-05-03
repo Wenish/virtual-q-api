3cb900525139be71b333419e8b9aaf465c8ba469
jonas.voland <jonas.voland@students.ffhs.ch>
2024-05-03 16:05:09 +0200

    feat: adding rest_framework_simplejwt

--- digitalqueues/settings.py
+++ digitalqueues/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders',
 ]
 
@@ -72,6 +72,13 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
 
 WSGI_APPLICATION = 'digitalqueues.wsgi.application'
 
