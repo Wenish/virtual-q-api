5d7fbb2027f8dc3ec1c65c760b49622ebbf8289d
jonas.voland <jonas.voland@students.ffhs.ch>
2024-05-03 22:57:51 +0200

    feat: check if user has permission to edit ticket and is owner of queue of the ticket

--- api/permissions.py
+++ api/permissions.py
@@ -3,3 +3,7 @@ from rest_framework.permissions import BasePermission
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+    
+class IsQueueOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.queue.user == request.user
--- api/views.py
+++ api/views.py
@@ -1,9 +1,9 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, exceptions
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Queue, Ticket
 from .serializers import QueueSerializer, TicketSerializer
-from .permissions import IsOwner
+from .permissions import IsOwner, IsQueueOwner
 
 class QueueViewSet(viewsets.ModelViewSet):
     # pylint: disable-next=no-member
@@ -22,4 +22,10 @@ class TicketViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['status', 'queue_id', 'user__id']
     authentication_classes = [JWTAuthentication]
-    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    permission_classes = [permissions.IsAuthenticated, IsOwner|IsQueueOwner]
+
+    def perform_update(self, serializer):
+        ticket_instance = serializer.instance
+        if ticket_instance.queue.user != self.request.user:
+            raise exceptions.PermissionDenied("You don't have permission to edit this ticket.")
+        serializer.save()
