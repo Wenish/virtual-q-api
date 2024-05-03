3c7a3b0bebbda32fd403284263372cdcf283e0b6
jonas.voland <jonas.voland@students.ffhs.ch>
2024-05-03 18:42:52 +0200

    feat: adding filters

--- api/views.py
+++ api/views.py
@@ -3,10 +3,14 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Queue, Ticket
 from .serializers import QueueSerializer, TicketSerializer
 from .permissions import IsOwner
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 class QueueViewSet(viewsets.ModelViewSet):
     queryset = Queue.objects.all()
     serializer_class = QueueSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'user__id']
     authentication_classes = [JWTAuthentication]
     permission_classes = [permissions.IsAuthenticated, IsOwner]
     
@@ -14,5 +18,7 @@ class QueueViewSet(viewsets.ModelViewSet):
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status', 'queue_id', 'user__id']
     authentication_classes = [JWTAuthentication]
     permission_classes = [permissions.IsAuthenticated, IsOwner]
--- digitalqueues/settings.py
+++ digitalqueues/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
@@ -79,7 +80,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 
